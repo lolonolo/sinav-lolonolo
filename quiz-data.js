30b96d65c4aa28bcc289d5d9bce1f51e6d090695
lolonolo <80510932+lolonolo@users.noreply.github.com>
2025-08-21 14:41:10 +0300

    Update quiz-data.js

--- quiz-data.js
+++ quiz-data.js
@@ -61,5 +61,197 @@ const sinavVerisi = {
             dogruCevapIndex: 2, // C şıkkı
             aciklama: "Cevap : C) 9"
         }
+		 {
+            soruMetni: "Poliçedeki sigorta bedelinin, sigorta konusunun gerçek ve makul değerinin üzerinde olması hali hangisidir?",
+            secenekler: ["A) Eksik sigorta", "B) Aşkın sigorta", "C) Birlikte sigorta", "D) Zorunlu sigorta"],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) Aşkın Sigorta"
+        },
+        {
+            soruMetni: "“Ortaklaşa konulmuş bir paranın taraflar arasında paylaşıldığı ve ortaklardan biri ölünce ölen kişinin parasının diğerine geçtiği müşterek sigorta türüdür”. Tanımlanan bu sigorta türü aşağıdakilerden hangisidir?",
+            secenekler: ["A) Yatırım fonlu sigortalar", "B) Tontin", "C) Sermaye itfa sigortası", "D) Birikimli hayat sigortası", "E) Doğum sigortası"],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) Tontin"
+        },
+        {
+            soruMetni: "Ferdi Kaza Sigortası bulunan Ahmet Bey, bir gün evinde otururken aniden kalp krizi geçirir. Kalp krizi sırasında dengesini kaybederek balkon korkuluğuna çarpar ve balkondan düşerek hayatını kaybeder. Ahmet Bey’in yakınları, Ferdi Kaza Sigortası’ndan tazminat almak için sigorta şirketine başvurur. Bu durumda Ahmet Bey’in yakınları, Ferdi Kaza Sigortası kapsamında tazminat alabilir mi?",
+            secenekler: ["A) Evet, düşme sonucu hayatını kaybettiği için tazminat ödenir.", "B) Hayır, çünkü ölümün ana nedeni kalp krizidir ve bu sağlık sorunu olduğundan tazminat ödenmez.", "C) Evet, çünkü düşme olayı ani ve dışsal bir etkendir.", "D) Hayır, çünkü sigorta sadece trafik kazası gibi dışsal etkileri kapsar."],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) Hayır, çünkü ölümün ana nedeni kalp krizidir ve bu sağlık sorunu olduğundan tazminat ödenmez."
+        },
+        {
+            soruMetni: "Aşağıdaki olaylardan hangisinde tazminat Güvence Hesabından karşılanmaz?",
+            secenekler: ["A) Ali, otoparkta park halinde duran aracına bir başka aracın çarpması sonucu hasarını karşılayamamış ve Güvence Hesabı'na başvurarak zararını tazmin ettirmiştir", "B) Duygu, karşıdan karşıya geçerken çalıntı bir aracın çarpması sonucu yaralanmış ve zararlarını Güvence Hesabı karşılamıştır.", "C) Ahmet, iflas eden bir sigorta şirketinden kasko sigortası yaptırdığı aracının çalınması sonucu zararını talep etmiştir.", "D) Zeynep, trafik sigortası olmayan biriyle karıştığı kazada, karşı tarafın kimlik bilgilerine ve trafik sigortasına ulaşamadığı için Güvence Hesabı'na başvurmuştur."],
+            dogruCevapIndex: 2, // C şıkkı
+            aciklama: "Cevap : C) Ahmet, iflas eden bir sigorta şirketinden kasko sigortası yaptırdığı aracının çalınması sonucu zararını talep etmiştir."
+        },
+        {
+            soruMetni: "Aşağıdakilerden hangisi Zorunlu Deprem Sigortası Teminat Kapsamına girmez?",
+            secenekler: ["A) Köy nüfusuna kayıtlı ve köyde sürekli oturanlarca köy yerleşik alanları ve civarında ve mezralarda yapılan binalar", "B) Tapuya kayıtlı ve özel mülkiyete tabi taşınmazlar üzerinde mesken olarak inşa edilmiş binalar", "C) Kat irtifakı tesis edilmiş binalar", "D) Tapu tahsisi henüz yapılmamış kooperatif evleri"],
+            dogruCevapIndex: 0, // A şıkkı
+            aciklama: "Cevap : A) Köy nüfusuna kayıtlı ve köyde sürekli oturanlarca köy yerleşik alanları ve civarında ve mezralarda yapılan binalar"
+        },
+        {
+            soruMetni: "Sigortacılık Kapsamında Değerlendirilecek Faaliyetler ve Mesafeli Akdedilen Sigorta Sözleşmeleri Yönetmeliğinde faaliyette bulunan kurum/kuruluşlar hangisidir?",
+            secenekler: ["A) Sigorta şirketleri, sigorta acenteleri, sigorta brokerları, finansal hizmet sağlayıcıları", "B) Sigorta şirketleri, reasürans şirketleri, sigorta teminatı veren emeklilik şirketleri, sigorta uzmanları, finansal danışmanlık firmaları", "C) Sigorta şirketleri, reasürans şirketleri, sigorta teminatı veren emeklilik şirketleri, sigorta acenteleri, bankalar ile özel kanunla kurulmuş ve kendisine sigorta acenteliği yapma yetkisi tanınan kurumlar", "D) Sigorta şirketleri, reasürans şirketleri, sigorta teminatı veren emeklilik şirketleri, sigorta aracıları, bankalar ile özel kanunla kurulmuş ve kendisine sigorta acenteliği yapma yetkisi tanınan kurumlar, mal ve hizmete bağlı sunulan sigortalar için ticaret ortamı sağlayan kişiler"],
+            dogruCevapIndex: 3, // D şıkkı
+            aciklama: "Cevap : D) Sigorta şirketleri, reasürans şirketleri, sigorta teminatı veren emeklilik şirketleri, sigorta aracıları, bankalar ile özel kanunla kurulmuş ve kendisine sigorta acenteliği yapma yetkisi tanınan kurumlar, mal ve hizmete bağlı sunulan sigortalar için ticaret ortamı sağlayan kişiler"
+        },
+        {
+            soruMetni: "Sigortacı, sözleşmenin yapılmasından itibaren poliçeyi sigortalıya bildirme süresi ne kadardır?",
+            secenekler: ["A) 24 saat", "B) 5 gün", "C) 15 gün", "D) bildirmek zorunda değil."],
+            dogruCevapIndex: 0, // A şıkkı
+            aciklama: "Cevap : A) 24 saat"
+        },
+        {
+            soruMetni: "Sigorta şirketlerinin üstlendikleri riskleri reasürans şirketlerine devretmesi işlemine “reasürans” denir. Ancak, reasürans şirketlerinin de kendi üzerindeki riski başka bir reasürans şirketine devretmesine ne ad verilir?",
+            secenekler: ["A) Koasürans", "B) Retrosesyon", "C) Tekafül", "D) Prim Transferi"],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) Retrosesyon"
+        },
+        {
+            soruMetni: "Can sigortalarında, sigorta sözleşmesine taraf olmamakla birlikte lehine sigorta sözleşmesi akdedilen ve rizikonun gerçekleşmesi hâlinde kural olarak sigorta tazminatını sigortacıdan isteme hakkına sahip olan kişiyi ifade eder?",
+            secenekler: ["A) Sigrota Ettiren", "B) Lehtar", "C) Sigortacı", "D) Aracı"],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) Lehtar"
+        },
+        {
+            soruMetni: "Mustafa Kemal Atatürk döneminde kurulan sigorta şirketi aşağıdakilerden hangisidir?",
+            secenekler: ["A) Anadolu Sigorta", "B) Halk Sigorta", "C) Türkiye Sigorta Birliği", "D) Milli Sigorta"],
+            dogruCevapIndex: 0, // A şıkkı
+            aciklama: "Cevap : A) Anadolu Sigorta"
+        }
+		{
+            soruMetni: "Türkiye’de trafik sigortası poliçesinin yenileme süresi geçtikten sonra her ay için uygulanan gecikme zammı yüzdesi nedir?",
+            secenekler: ["A) yüzde 3", "B) yüzde 5", "C) yüzde 10", "D) yüzde 15"],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) yüzde 5"
+        },
+        {
+            soruMetni: "Aktüerlerle ilgili aşağıdaki bilgilerden hangisi yanlıştır?",
+            secenekler: ["A) Sigorta şirketleri, yeterli sayıda aktüerle çalışmak zorundadır.", "B) Aktüerler 5684 sayılı Sigortacılık Kanununda düzenlenmektedir.", "C) Hazine Müsteşarlığı tarafından aktüerlerin kaydedildiği bir Aktüerler Sicili tutulur.", "D) Aktüerlerin ruhsatı Aktüerler Derneği tarafından verilir."],
+            dogruCevapIndex: 3, // D şıkkı
+            aciklama: "Cevap : D) Aktüerlerin ruhsatı Aktüerler Derneği tarafından verilir."
+        },
+        {
+            soruMetni: "“İş planı: Sigorta şirketleri ile reasürans şirketlerinin kuruluş amacı ile en az ilk ________ yıldaki faaliyetlerine ilişkin tahminlerini ve yükümlülüklerini sürekli olarak yerine getirebileceğini ayrıntılı bir şekilde ortaya koyan planı,”<br>Aşağıdaki tanımda boş bırakılan yere gelmesi gereken ifade hangisidir?",
+            secenekler: ["A) 1", "B) 2", "C) 3", "D) 5"],
+            dogruCevapIndex: 2, // C şıkkı
+            aciklama: "Cevap : C) 3"
+        },
+        {
+            soruMetni: "Sigortacılık tekniği ile buna ilişkin yatırım, finansman ve demografi konularında olasılık ve istatistik teorilerini uygulayarak, yasal düzenlemelere uygun prim, karşılık ve kâr paylarını hesaplayan, tarife ve teknik esasları hazırlayana ne ad verilir?",
+            secenekler: ["A) Sigortacı", "B) Aracı", "C) Aktüer", "D) Broker"],
+            dogruCevapIndex: 2, // C şıkkı
+            aciklama: "Cevap : C) Aktüer"
+        },
+        {
+            soruMetni: "Poliçenin bir bölümü veya poliçeye ekli olarak, taraflar arasındaki sigorta sözleşmesinin kapsamını daraltmak, genişletmek ya da değiştirmek amacıyla hazırlanan ek hükümlere ne ad verilir?",
+            secenekler: ["A) Kloz", "B) İbraname", "C) Genel Şartlar", "D) Tecditname"],
+            dogruCevapIndex: 0, // A şıkkı
+            aciklama: "Cevap : A) Kloz"
+        },
+        {
+            soruMetni: "Aşağıdakilerden hangisi veya hangileri kasko sigortasında teminat dışında kalan zararlardandır?<br>I.Savaş olayları, düşman hareketleri sonucu meydana gelen zararlar<br>II.Aracın uyuşturucu madde ile kullanılması sonucu meydana gelen zararlar<br>III.Poliçedeki ilgili aracın sürücü belgesi olmayan kimseler tarafından kullanılması sonucu meydana gelen zararlardır?",
+            secenekler: ["A) I,II ve III", "B) I ve II", "C) I ve III", "D) II ve III"],
+            dogruCevapIndex: 0, // A şıkkı
+            aciklama: "Cevap : A) I,II ve III"
+        },
+        {
+            soruMetni: "Paranın peşin malın veresiye olduğu sözleşme türü aşağıdakilerden hangisidir?",
+            secenekler: ["A) İstisna", "B) Selem", "C) İcare", "D) Sarf"],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) Selem"
+        },
+        {
+            soruMetni: "Bankaların yaptıkları ipotek işlemleri hangi sözleşme kapsamına girmektedir?",
+            secenekler: ["A) Havale", "B) Hibe", "C) Rehin", "D) Sarf"],
+            dogruCevapIndex: 2, // C şıkkı
+            aciklama: "Cevap : C) Rehin"
+        },
+        {
+            soruMetni: "Türkiye Sigorta, Reasürans ve Emeklilik Şirketleri Birliği’nin (TSB) Genel Kurulu’nun yetkileri arasında aşağıdakilerden hangisi bulunur?",
+            secenekler: ["A) Sigorta şirketlerinin denetimini yapmak", "B) Sigorta poliçelerinin fiyatlarını belirlemek", "C) Sigortacılık ile ilgili tüzel kişiliğe sahip kuruluşlar kurmak veya mevcut kuruluşlara iştirak etmek", "D) Sigortalılar ile sigorta şirketleri arasındaki uyuşmazlıkları çözmek"],
+            dogruCevapIndex: 2, // C şıkkı
+            aciklama: "Cevap : C) Sigortacılık ile ilgili tüzel kişiliğe sahip kuruluşlar kurmak veya mevcut kuruluşlara iştirak etmek"
+        },
+        {
+            soruMetni: "Kendi isteğiyle faaliyetine son vermek isteyen acentelerin, bu durumu kararın alındığı tarihten itibaren kaç gün içinde Levha bilgilerine işlenmek üzere TOBB’a bildirmesi gerekmektedir?",
+            secenekler: ["a) 3 iş günü", "b) 5 iş günü", "c) 7 iş günü", "d) 10 iş günü"],
+            dogruCevapIndex: 1, // b şıkkı
+            aciklama: "Cevap : b) 5 iş günü"
+        }
+		{
+            soruMetni: "Sigorta acentelerinin şube açmaları durumunda, her bir şube için asgari sermaye tutarı ne kadar olmalıdır?",
+            secenekler: ["A) 250.000 TL", "B) 300.000 TL", "C) 325.000 TL", "D) 350.000 TL"],
+            dogruCevapIndex: 2, // C şıkkı
+            aciklama: "Cevap : C) 325.000 TL"
+        },
+        {
+            soruMetni: "Sigortalıya karşı üstlendiği sigortanın bir kısmını veya tamamını reasürans yoluyla devreden şirkete ne ad verilir?",
+            secenekler: ["A) Reasüans", "B) Sedan", "C) Broker", "D) Sigortacı"],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) Sedan"
+        },
+        {
+            soruMetni: "Aşağıdakilerden hangileri, sigorta bilgilendirme formunda asgari olarak bulunması gereken bilgilerdendir?<br>I. Sigortacının ticaret unvanı ve iletişim bilgileri<br>II. Sigorta priminin ödenme zamanı ve şekli<br>III. Rizikonun gerçekleşmesi durumunda yapılması gerekenler<br>IV. Sigortalının medeni hali ve aile bilgileri",
+            secenekler: ["A) Yalnız I ve II", "B) I, II ve III", "C) II, III ve IV", "D) I, III ve IV"],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) I, II ve III"
+        },
+        {
+            soruMetni: "Sigorta şirketinin, hasarı sigortalıya ödeyince, sigortalının yerine geçerek, hasara sebep olan kusurlu taraftan sigortalının hakkı kadar ve en çok ödediği hasar kadar alacaklı olması hangi prensipten kaynaklanır?",
+            secenekler: ["A) Tazminat", "B) Halefiyet", "C) Azami iyi niyet", "D) Yakın sebep"],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) Halefiyet"
+        },
+        {
+            soruMetni: "Aşağıdakilerden hangisi, sigorta uygulamaları da dahil olmak üzere, risk değerlendirmesine esas bilgileri toplamak ve bu bilgileri sigorta, reasürans ve sigortacılık faaliyetinde bulunan emeklilik şirketleri ile Türkiye Cumhuriyeti Hazine ve Maliye Bakanlığı’nın belirlediği kişilerle paylaşılmasını sağlamak amacıyla Türkiye Sigorta ve Reasürans Şirketleri Birliği bünyesinde tüzel kişiliği haiz bir kurum olarak kurulmuştur?",
+            secenekler: ["A) SBM (Sigorta Bilgi ve Gözetim Merkezi)", "B) SDDK (Sigortacılık ve Özel Emeklilik Düzenleme ve Denetleme Kurumu)", "C) SPK (Sermaye Piyasası Kurulu)", "D) TOBB (Türkiye Odalar ve Borsalar Birliği)"],
+            dogruCevapIndex: 0, // A şıkkı
+            aciklama: "Cevap : A) SBM (Sigorta Bilgi ve Gözetim Merkezi)"
+        },
+        {
+            soruMetni: "3 yıldır çalışan 4 yıllık İktisat bölümü mezunu bir kişinin sigorta sektöründe genel müdür olabilmesi için toplamda kaç yıl daha çalışmış olması gerekmektedir?",
+            secenekler: ["A) 3 yıl", "B) 5 yıl", "C) 6 yıl", "D) 7 yıl"],
+            dogruCevapIndex: 0, // A şıkkı
+            aciklama: "Cevap : A) 3 yıl"
+        },
+        {
+            soruMetni: "Fıkıh kitaplarındaki finansal ve ticari işlemler ile alakalı kurallar hangi başlık altında toplanmıştır?",
+            secenekler: ["A) Muamelat", "B) Münakehat", "C) İbadet", "D) İtikad"],
+            dogruCevapIndex: 0, // A şıkkı
+            aciklama: "Cevap : A) Muamelat"
+        },
+        {
+            soruMetni: "Yardımcı teknik personelin teknik personel ünvanını kazanmak için doldurması gereken çalışma süresi ne kadardır?",
+            secenekler: ["A) 3 ay", "B) 6 ay", "C) 1 yıl", "D) 2 yıl"],
+            dogruCevapIndex: 3, // D şıkkı
+            aciklama: "Cevap : D) 2 yıl"
+        },
+        {
+            soruMetni: "Muamelat fıkhı çerçevesinde bir sözleşme kuruluşunda ilk teklifi ifade ve irade beyanına ne denir?",
+            secenekler: ["A) Kabul", "B) İcap", "C) İcma", "D) Makbul"],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) İcap"
+        },
+        {
+            soruMetni: "Yangın sigorta poliçesinde aşağıdakilerden hangisi teminat dışındadır?",
+            secenekler: ["A) Yangın esnasında çıkan duman nedeniyle oluşan hasarlar", "B) Yangın esnasında çıkan buhar nedeniyle oluşan hasarlar", "C) Yangın söndürme esnasında sıkılan su nedeniyle oluşan hasarlar", "D) Savaş, istila, isyan sonucu oluşan hasarlar"],
+            dogruCevapIndex: 3, // D şıkkı
+            aciklama: "Cevap : D) Savaş, istila, isyan sonucu oluşan hasarlar"
+        },
+        {
+            soruMetni: "Aşağıdakilerden hangisi “karz” teriminin doğru tanımıdır?",
+            secenekler: ["A) Sigorta poliçelerinde ek hüküm veya özel şarttır.", "B) Faizsiz borç verme işlemidir.", "C) Reasürans sözleşmesinde kullanılan bir terimdir.", "D) Sigortalı ile sigorta şirketi arasındaki sözleşmedir."],
+            dogruCevapIndex: 1, // B şıkkı
+            aciklama: "Cevap : B) Faizsiz borç verme işlemidir."
+        },
+        {
+            soruMetni: "Risk yönetimi ile ilgili olarak aşağıdakilerden hangisi doğrudur?",
+            secenekler: ["A) Ürün kalitesini artırmaya yönelik çalışmalar risk yönetimi kapsamında değildir.", "B) Risk yönetimi yalnızca finansal riskleri kapsar.", "C) Ürün kalitesini artırmak, risk yönetiminin bir parçasıdır.", "D) Risk yönetimi sadece doğal afetleri kapsar."],
+            dogruCevapIndex: 2, // C şıkkı
+            aciklama: "Cevap : C) Ürün kalitesini artırmak, risk yönetiminin bir parçasıdır."
+        }
     ]
 };
